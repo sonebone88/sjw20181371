356b69eb4bfc5845926738477e17a5db19e89aa6
sjw <sonebone88@gmail.com>
2021-03-25 07:33:31 +0000

    first commit

---
+++ 4wk_python.py
@@ -0,0 +1,21 @@
+import random #랜덤 라이브러리 호출 - random.shuffle()식으로 사용
+# 주석 ctrl /, """
+"""
+프로그램 사이 주석(인덴드 중요)
+"""
+
+print("Hello world") #프린트문 != printf from C
+
+def abc(x:int,y:int):#함수
+    return x+y
+for f in range(6):#포문
+    print("반복문")
+    
+project_team =[]#list
+# 리스트에 추가project_team.append()abc()
+"""
+ 터미널에: git init - git에서 관리하는 파일
+ git add . - 폴더 안에 있는 파일들을 관리하는 버전에 올리기
+ git commit -m "first commit" 이라고 저장해줘
+ -> 이메일 주소 입력, 유저이름 입력 필요력 필요
+"""
